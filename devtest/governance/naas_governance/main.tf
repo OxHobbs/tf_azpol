0ce61ee355e93ce1229f936592c261c5187f417e
Austin Hobbs <austin.hobbs@microsoft.com>
2020-03-05 10:33:56 -0500

    updated yaml

--- devtest/governance/naas_governance/main.tf
+++ devtest/governance/naas_governance/main.tf
@@ -1,15 +1,15 @@
-variable "client_id" {}
-variable "tenant_id" {}
-variable "client_secret" {}
-variable "subscription_id" {}
+# variable "client_id" {}
+# variable "tenant_id" {}
+# variable "client_secret" {}
+# variable "subscription_id" {}
 
 provider "azurerm" {
   version         = "=1.37.0"
   environment     = "usgovernment"
-  client_id       = var.client_id
-  tenant_id       = var.tenant_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # tenant_id       = var.tenant_id
+  # client_secret   = var.client_secret
+  # subscription_id = var.subscription_id
 }
 
 
